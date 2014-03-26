c6562f0ed87ba03341939fdb80897cc90251aa6f
saorin <phansaorin@gmail.com>
2014-03-26 16:07:50 +0700

    start to push project to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotVoting::Application.config.secret_key_base = '6561f29cee5bef6252202771e4abf0bd7acbc1bbb65ff33238b04c4565a0372073a65113cf5946343f078802776db6b2a4640bff98a91defc10598ec8ea4c85e'
