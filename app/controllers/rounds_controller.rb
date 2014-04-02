61af2e2dadd4e3b246877b21841dba4d4ed5323e
saorin <phansaorin@gmail.com>
2014-04-02 13:35:06 +0700

    edit of round

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -16,7 +16,24 @@ class RoundsController < ApplicationController
       @round.save
 	   redirect_to rounds_path
   end
-
+  def edit
+    @round = Round.find_by_id(params[:id])
+    # add_breadcrumb "Home", users_path
+    # add_breadcrumb "Edit User"
+  end
+  def update
+      @round = Round.find_by_id(params[:id])
+      @round.question = params[:round][:question]
+      @round.suggestion = params[:round][:suggestion]
+      @round.deadline = params[:round][:deadline]
+    if @round.save
+      redirect_to rounds_path
+      flash.notice = "#{@round.question} has been updated!"
+    else 
+      render :edit
+      flash.alert = "Update failed! Please try again!"
+    end
+  end
   # private
   # def round_params
   #   params.require(:round).permit(:question, :suggestion, :deadline)
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,5 @@
 class Round < ActiveRecord::Base
+
+  # the default (validates on both create and update)
+  validates :question, :deadline, :suggestion , presence: true
 end
