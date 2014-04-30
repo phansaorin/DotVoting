2317196792b78dec90ff440c7ab274e956d03cc1
sokeara <sim.sokeara@gmail.com>
2014-04-30 15:55:57 +0700

    sokeara update vote answer

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140429092831) do
 
-  create_table "Users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "Users", ["email"], name: "index_users_on_email", unique: true
-  add_index "Users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   create_table "answers", force: true do |t|
     t.string   "txt_answers"
     t.integer  "round_id"
@@ -54,6 +36,24 @@ ActiveRecord::Schema.define(version: 20140429092831) do
     t.datetime "updated_at"
     t.boolean  "status",     default: false
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: true do |t|
     t.datetime "created_at"
