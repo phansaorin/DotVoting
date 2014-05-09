447e8d0258184f7af1a39ac379158e77e8e9c832
chhingchhing <chhingchhing.hem@gmail.com>
2014-05-09 16:45:00 +0700

    Delete all records of user voted at previous time

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,4 +4,13 @@ class Round < ActiveRecord::Base
   has_many :user_answers
   # the default (validates on both create and update)
   validates :question, :deadline, presence: true
+
+  def self.delete_user_answer user_id, round_id
+    # Delete all records of user voted at previous time
+	user_answers = UserAnswer.find_all_by_user_id_and_round_id(user_id, round_id)
+	user_answers.each do |u_answer|
+		u_answer.destroy
+	end
+  end
+
 end
